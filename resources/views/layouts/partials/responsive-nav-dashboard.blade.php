c1a46e2272d9bc256ce357a9cee4510228dd7ecf
KienNguyen1911 <ngkien1911@gmail.com>
2024-02-27 08:35:12 +0700

    feature/ui/dashboard

---
+++ resources/views/layouts/partials/responsive-nav-dashboard.blade.php
@@ -0,0 +1,21 @@
+<div class="dropdown">
+    <button onclick="myFunction()" class="dropbtn"><i class="fa fa-bars pr10"></i> Dashboard Navigation</button>
+    <ul id="myDropdown" class="dropdown-content">
+        <li class="active"><a href="page-dashboard.html"><i class="flaticon-discovery mr10"></i>Dashboard</a></li>
+        <li><a href="page-dashboard-message.html"><i class="flaticon-chat-1 mr10"></i>Message</a></li>
+        <li>
+            <p class="fz15 fw400 ff-heading mt30 pl30">MANAGE LISTINGS</p>
+        </li>
+        <li><a href="page-dashboard-add-property.html"><i class="flaticon-new-tab mr10"></i>Add New Property</a></li>
+        <li><a href="page-dashboard-properties.html"><i class="flaticon-home mr10"></i>My Properties</a></li>
+        {{-- <li><a href="page-dashboard-favorites.html"><i class="flaticon-like mr10"></i>My Favorites</a></li>
+        <li><a href="page-dashboard-savesearch.html"><i class="flaticon-search-2 mr10"></i>Saved Search</a></li>
+        <li><a href="page-dashboard-review.html"><i class="flaticon-review mr10"></i>Reviews</a></li> --}}
+        <li>
+            <p class="fz15 fw400 ff-heading mt30 pl30">MANAGE ACCOUNT</p>
+        </li>
+        {{-- <li><a href="page-dashboard-package.html"><i class="flaticon-protection mr10"></i>My Package</a></li> --}}
+        <li><a href="page-dashboard-profile.html"><i class="flaticon-user mr10"></i>My Profile</a></li>
+        <li><a class="" href="page-login.html"><i class="flaticon-exit mr10"></i>Logout</a></li>
+    </ul>
+</div>
---
+++ resources/views/layouts/partials/sidebar-list-dashboard.blade.php
@@ -0,0 +1,44 @@
+<div class="dashboard_sidebar_list">
+    <div class="sidebar_list_item">
+        <a href="page-dashboard.html" class="items-center -is-active"><i
+                class="flaticon-discovery mr15"></i>Dashboard</a>
+    </div>
+    {{-- <div class="sidebar_list_item ">
+        <a href="page-dashboard-message.html" class="items-center"><i
+                class="flaticon-chat-1 mr15"></i>Message</a>
+    </div> --}}
+    <p class="fz15 fw400 ff-heading mt30">MANAGE LISTINGS</p>
+    <div class="sidebar_list_item ">
+        <a href="page-dashboard-add-property.html" class="items-center"><i
+                class="flaticon-new-tab mr15"></i>Add New Property</a>
+    </div>
+    <div class="sidebar_list_item ">
+        <a href="page-dashboard-properties.html" class="items-center"><i
+                class="flaticon-home mr15"></i>My Properties</a>
+    </div>
+    {{-- <div class="sidebar_list_item ">
+        <a href="page-dashboard-favorites.html" class="items-center"><i
+                class="flaticon-like mr15"></i>My Favorites</a>
+    </div>
+    <div class="sidebar_list_item ">
+        <a href="page-dashboard-savesearch.html" class="items-center"><i
+                class="flaticon-search-2 mr15"></i>Saved Search</a>
+    </div>
+    <div class="sidebar_list_item ">
+        <a href="page-dashboard-review.html" class="items-center"><i
+                class="flaticon-review mr15"></i>Reviews</a>
+    </div> --}}
+    <p class="fz15 fw400 ff-heading mt30">MANAGE ACCOUNT</p>
+    {{-- <div class="sidebar_list_item ">
+        <a href="page-dashboard-package.html" class="items-center"><i
+                class="flaticon-protection mr15"></i>My Package</a>
+    </div> --}}
+    <div class="sidebar_list_item ">
+        <a href="page-dashboard-profile.html" class="items-center"><i
+                class="flaticon-user mr15"></i>My Profile</a>
+    </div>
+    <div class="sidebar_list_item ">
+        <a href="page-login.html" class="items-center"><i
+                class="flaticon-logout mr15"></i>Logout</a>
+    </div>
+</div>
