3536981f9d56a5d67dfc9a74384e8c167c648621
KienNguyen1911 <duypk@thefirstone.jp>
2024-03-11 19:43:19 +0700

    feature/content-page: change content page in VN

--- resources/views/components/info-user.blade.php
+++ resources/views/components/info-user.blade.php
@@ -1,10 +1,10 @@
-<section class="pb-0">
+<section class="pb-0" id="form-info">
     <div class="container">
         <div class="row">
             <div class="col-lg-6 col-xl-5 wow fadeInRight" data-wow-delay="00ms">
                 <div class="main-title mb40">
-                    <h2 class="title">Real Estate Inquiry Form</h2>
-                    <p class="paragraph fz15">As the complexity of buildings to increase</p>
+                    <h2 class="title">HÃY ĐỂ CHÚNG TÔI GIÚP BẠN</h2>
+                    <p class="paragraph fz15">Sự hài lòng của bạn là niềm vui của chúng tôi</p>
                 </div>
                 <div class="inquiry-form mb30-md">
                     <form class="form-style1" method="POST" action="{{ route('user-info') }}">
@@ -51,37 +51,37 @@
                             </div> --}}
                             <div class="col-md-12">
                                 <div class="mb20">
-                                    <label class="form-label fw600 dark-color">Personel Name</label>
-                                    <input type="text" class="form-control" placeholder="Your Name" name="name"
+                                    <label class="form-label fw600 dark-color">Họ và tên</label>
+                                    <input type="text" class="form-control" placeholder="Tên của bạn" name="name"
                                         value="{{ old('name') }}">
                                 </div>
                             </div>
                             <div class="col-md-7">
                                 <div class="mb20">
                                     <label class="form-label fw600 dark-color">Email</label>
-                                    <input type="email" class="form-control" placeholder="creativelayers088@gmail.com"
+                                    <input type="email" class="form-control" placeholder="info@t-dolphin.com"
                                         name="email" value="{{ old('email') }}">
                                 </div>
                             </div>
                             <div class="col-md-5">
                                 <div class="mb20">
-                                    <label class="form-label fw600 dark-color">Phone number</label>
+                                    <label class="form-label fw600 dark-color">Số điện thoại</label>
                                     <input type="text" class="form-control" placeholder="0912341234" name="phone"
                                         value="{{ old('phone') }}">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="mb20">
-                                    <label class="form-label fw600 dark-color">Province</label>
+                                    <label class="form-label fw600 dark-color">Tỉnh</label>
                                     <div class="bootselect-multiselect">
                                         <select class="selectpicker" name="province_id" id="province_id">
-                                            <option value="">Please, select province</option>
+                                            <option value="">Chọn tỉnh</option>
                                             @forelse ($provinces as $province)
                                                 <option value="{{ $province->id }}"
                                                     @if (old('province_id') == $province->id) selected @endif>
                                                     {{ $province->name }}</option>
                                             @empty
-                                                <option>No Province</option>
+                                                <option>Không có dữ liệu</option>
                                             @endforelse
                                         </select>
                                     </div>
@@ -89,23 +89,24 @@
                             </div>
                             <div class="col-md-6">
                                 <div class="mb20">
-                                    <label class="form-label fw600 dark-color">District</label>
+                                    <label class="form-label fw600 dark-color">Quận/Huyện</label>
                                     <div class="bootselect-multiselect">
                                         <select class="selectpicker" multiple name="district_ids[]" id="district_id">
+                                            <option value="">Chọn quận/huyện</option>
                                         </select>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="mb30">
-                                    <label class="form-label fw600 dark-color">Min Price</label>
+                                    <label class="form-label fw600 dark-color">Giá tối thiểu</label>
                                     <input type="text" class="form-control" placeholder="VND 1.000.000" name="min_price"
                                         value="{{ old('min_price') }}">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="mb20">
-                                    <label class="form-label fw600 dark-color">Max Price</label>
+                                    <label class="form-label fw600 dark-color">Giá tối đa</label>
                                     <input type="text" class="form-control" placeholder="VND 10.000.000" name="max_price"
                                         value="{{ old('max_price') }}">
                                 </div>
@@ -113,13 +114,13 @@
                             {{-- textarea --}}
                             <div class="col-md-12">
                                 <div class="mb20">
-                                    <label class="form-label fw600 dark-color">Message</label>
-                                    <textarea class="form-control" placeholder="Write your message" name="note"
+                                    <label class="form-label fw600 dark-color">Ghi chú</label>
+                                    <textarea class="form-control" placeholder="Hãy cho chúng tôi biết mong muốn của bạn" name="note"
                                         value="{{ old('note') }}"></textarea>
                                 </div>
                             </div>
                             <div class="d-grid">
-                                <button class="ud-btn btn-thm" type="submit">Submit <i
+                                <button class="ud-btn btn-thm" type="submit">Xác nhận<i
                                         class="fal fa-arrow-right-long"></i></button>
                             </div>
                         </div>
@@ -135,3 +136,9 @@
         </div>
     </div>
 </section>
+
+<style>
+    .filter-option-inner-inner {
+        font-size: 14px;
+    }
+</style>
---
+++ resources/views/components/testimonials.blade.php
@@ -0,0 +1,161 @@
+<section class="pb30-md bgc-f7">
+    <div class="container">
+        <div class="row align-items-md-center">
+            <div class="col-lg-6 mb30-md wow fadeInUp" data-wow-delay="100ms">
+                <div class="main-title">
+                    <h2 class="title">Khách Hàng Của Chúng Tôi</h2>
+                    <p class="paragraph">Luôn mang trải nghiệm tốt nhất cho người dùng</p>
+
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="funfact_one">
+                            <div class="details">
+                                <ul class="ps-0 d-flex mb-0">
+                                    <li>
+                                        <div class="timer">87</div>
+                                    </li>
+                                    <li><span>%</span></li>
+                                </ul>
+                                <p class="text mb-0">Phản Hồi</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="funfact_one">
+                            <div class="details">
+                                <ul class="ps-0 d-flex mb-0">
+                                    <li>
+                                        <div class="timer">99</div>
+                                    </li>
+                                    <li><span>%</span></li>
+                                </ul>
+                                <p class="text mb-0">Hài Lòng</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="funfact_one">
+                            <div class="details">
+                                <ul class="ps-0 d-flex mb-0">
+                                    <li>
+                                        <div class="timer">80</div>
+                                    </li>
+                                    <li><span>%</span></li>
+                                </ul>
+                                <p class="text mb-0">Đánh Giá</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-xl-4 offset-xl-2">
+                <div class="testimonial-slider2 navi_pagi_bottom_center slider-1-grid owl-carousel owl-theme wow fadeInUp"
+                    data-wow-delay="300ms">
+                    <div class="item">
+                        <div class="testimonial-style1 position-relative mb25">
+                            <div class="testimonial-content">
+                                <h5 class="title">Tuyệt Vời</h5>
+                                <span class="icon fas fa-quote-left"></span>
+                                <p class="text">Tôi đã có trải nghiệm tuyệt vời khi thuê phòng tại các dịch vụ ở Hà Nội. Nhân viên phục vụ rất thân thiện và chuyên nghiệp. Phòng được thiết kế đẹp mắt và sạch sẽ. Tiện ích như wifi, điều hòa hoạt động tốt, giúp cho việc lưu trú trở nên thoải mái và tiện lợi hơn bao giờ hết.”</p>
+                                <div class="testimonial-review">
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a href="#"><i class="fas fa-star"></i></a>
+                                </div>
+                            </div>
+                            <div class="thumb d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <img height="50" class="wa rounded-circle" src="images/testimonials/testimonial-6.jpg" alt="">
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h6 class="mb-0">Mẫn Nhi</h6>
+                                    <p class="mb-0">Hà Nội</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimonial-style1 position-relative mb25">
+                            <div class="testimonial-content">
+                                <h5 class="title">Xuất Sắc</h5>
+                                <span class="icon fas fa-quote-left"></span>
+                                <p class="text">Dịch vụ cho thuê phòng tại TP.HCM mang lại sự thoải mái và tiện nghi tuyệt vời. Phòng được trang bị đầy đủ các tiện ích hiện đại như TV màn hình phẳng, minibar, và phòng tắm riêng. Vị trí thuận lợi giúp cho việc di chuyển và khám phá thành phố dễ dàng hơn. Tôi rất hài lòng với dịch vụ này.”</p>
+                                <div class="testimonial-review">
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a href="#"><i class="fas fa-star"></i></a>
+                                </div>
+                            </div>
+                            <div class="thumb d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <img height="50" class="wa rounded-circle" src="images/testimonials/testimonial-5.jpg" alt="">
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h6 class="mb-0">Như Ngọc</h6>
+                                    <p class="mb-0">TP.HCM</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimonial-style1 position-relative mb25">
+                            <div class="testimonial-content">
+                                <h5 class="title">Hài Lòng</h5>
+                                <span class="icon fas fa-quote-left"></span>
+                                <p class="text">Tôi đã có một kỳ nghỉ tuyệt vời khi thuê phòng tại Hà Nội. Không chỉ là phòng ốc được bài trí đẹp mắt và tiện nghi, mà còn là sự phục vụ tận tình và chu đáo từ đội ngũ nhân viên. Khám phá thành phố cổ Hà Nội từ căn phòng này là một trải nghiệm độc đáo và khó quên. Tôi chắc chắn sẽ giới thiệu dịch vụ này cho bạn bè và gia đình của mình.”</p>
+                                <div class="testimonial-review">
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a href="#"><i class="fas fa-star"></i></a>
+                                </div>
+                            </div>
+                            <div class="thumb d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <img height="50" class="wa rounded-circle" src="images/testimonials/testimonial-4.jpg"
+                                        alt="">
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h6 class="mb-0">Trần Dương</h6>
+                                    <p class="mb-0">Hà Nội</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testimonial-style1 position-relative mb25">
+                            <div class="testimonial-content">
+                                <h5 class="title">Tin Tưởng</h5>
+                                <span class="icon fas fa-quote-left"></span>
+                                <p class="text">Thuê phòng ở Đà Nẵng là một trải nghiệm đáng nhớ. Không chỉ là những căn phòng sạch sẽ và tiện nghi, mà còn là sự tiếp đón nồng hậu và tận tình từ nhân viên. Tôi đã cảm thấy như ở nhà mình với không gian ấm cúng và những buổi tối thư giãn tuyệt vời. Nếu có cơ hội, tôi sẽ quay lại lựa chọn dịch vụ này một lần nữa.”</p>
+                                <div class="testimonial-review">
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a class="me-1" href="#"><i class="fas fa-star"></i></a>
+                                    <a href="#"><i class="fas fa-star"></i></a>
+                                </div>
+                            </div>
+                            <div class="thumb d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <img height="50" class="wa rounded-circle" src="images/testimonials/testimonial-7.jpg"
+                                        alt="">
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h6 class="mb-0">Bùi Nam</h6>
+                                    <p class="mb-0">Đà Nẵng</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
